3e1e74a3a553a7c05391b718fcee0bb678208663
Marwin <git@marwinh.de>
2019-06-17 02:42:53 +0200

    admin: master: door: script: move vue component definitions into own file

--- admin/master/door/script.js
+++ admin/master/door/script.js
@@ -1,3 +1,15 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+let door_status = clone(shif_device);
+door_status.template = `
+    <shif-generic-l2 v-bind:icon="cond.icon.name"
+                     v-bind:title="dev.label"
+                     v-bind:active="{icon: cond.icon.color, text: cond.text.color}"
+                     v-bind:status="status"
+                     v-bind:place="place">
+    </shif-generic-l2>
+`;
+
+shif_comps_create('doorContact', door_status, door_status);
+shif_comps_create('doorHandle',  door_status, door_status);
