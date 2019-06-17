063deaefac319f42a01dd4acbbe72cd6fcf52a7e
Marwin <git@marwinh.de>
2019-06-17 02:43:46 +0200

    admin: master: ventilation: script: move vue component definitions into own file

---
+++ admin/master/ventilation/script.js
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+let ventilation_mode_l2 = clone(shif_device);
+ventilation_mode_l2.template = `
+    <shif-generic-l2 v-bind:icon="icons.l2.name"
+                     v-bind:title="dev.label"
+                     v-bind:active="{icon: icons.l2.color, text: texts.title.color}"
+                     v-bind:place="place"
+                     v-bind:actions="true"
+                     v-bind:status="status"
+                     v-on:click="level3(device, breadcrumb)">
+    </shif-generic-l2>
+`;
+
+let ventilation_mode_l3 = clone(shif_device);
+ventilation_mode_l3.computed.values = function () {
+    return this.rendering
+        .map(x => ({
+            name:     x.definitions.texts.state.content,
+            value:    x.condition.value,
+            selected: x.condition.value == this.props.value,
+        }));
+};
+ventilation_mode_l3.template = `
+    <shif-radio v-bind:title="title"
+                v-bind:values="values"
+                v-on:input="x => $homegear.value_set(output, parseInt(x))">
+    </shif-radio>
+`;
+
+shif_comps_create('ventilationMode', ventilation_mode_l2, ventilation_mode_l3);
