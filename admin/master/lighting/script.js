64d8d7339f163352a8bcf11a17d4be1832d8798d
Marwin <git@marwinh.de>
2019-06-17 02:42:57 +0200

    admin: master: lighting: script: move vue component definitions into own file

--- admin/master/lighting/script.js
+++ admin/master/lighting/script.js
@@ -1,3 +1,71 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+let lighting_switch_l2 = clone(shif_device);
+lighting_switch_l2.template = `
+    <shif-generic-l2 v-bind:icon="cond.icon.name"
+                     v-bind:title="dev.label"
+                     v-bind:active="{icon: cond.icon.color, text: cond.text.color}"
+                     v-bind:status="status"
+                     v-bind:place="place"
+                     v-bind:actions="true"
+                     v-on:click_icon="$homegear.value_set(output, !props.value)"
+                     v-on:click="level3(device, breadcrumb)">
+    </shif-generic-l2>
+`;
+
+let lighting_switch_l3 = clone(shif_device);
+lighting_switch_l3.template = `
+    <shif-generic-l2 v-bind:icon="cond.icon.name"
+                     v-bind:title="title"
+                     v-bind:active="{icon: cond.icon.color, text: cond.text.color}"
+                     v-bind:place="place"
+                     v-bind:status="status"
+                     v-on:click="$homegear.value_set(output, !props.value)">
+    </shif-generic-l2>
+`;
+
+shif_comps_create('lightingSwitch', lighting_switch_l2, lighting_switch_l3);
+
+
+
+let lighting_brightness = clone(shif_device);
+lighting_brightness.template = `
+    <shif-slider v-bind:min="props.minimumScaled"
+                 v-bind:max="props.maximumScaled"
+                 v-bind:unit="props.unit"
+                 v-bind:value="props.value"
+                 v-bind:title="title"
+                 v-on:change="$homegear.value_set(output, props.value)"
+                 v-model:value="props.value">
+    </shif-slider>
+`;
+
+shif_comps_create('lightingBrightness', lighting_switch_l2, lighting_brightness);
+
+
+
+let lighting_button_l2 = clone(shif_device);
+lighting_button_l2.template = `
+    <shif-generic-l2 v-bind:icon="cond.icon.name"
+                     v-bind:title="dev.label"
+                     v-bind:active="{icon: cond.icon.color, text: cond.text.color}"
+                     v-bind:status="status"
+                     v-bind:place="place"
+                     v-on:click="level3(device, breadcrumb)">
+    </shif-generic-l2>
+`;
+
+let lighting_button_l3 = clone(shif_device);
+lighting_button_l3.template = `
+    <shif-generic-l2 v-bind:icon="cond.icon.name"
+                     v-bind:title="title"
+                     v-bind:active="{icon: cond.icon.color, text: cond.text.color}"
+                     v-bind:status="status"
+                     v-bind:place="place"
+                     v-on:mousedown="$homegear.value_set(output, true)"
+                     v-on:mouseup="$homegear.value_set(output, false)">
+    </shif-generic-l2>
+`;
+
+shif_comps_create('lightingButton', lighting_button_l2, lighting_button_l3);
