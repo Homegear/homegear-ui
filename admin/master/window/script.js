b3f5ad1eaa57d6430bcc0a0bd7f0066e3a14a173
Marwin <git@marwinh.de>
2019-06-17 02:43:11 +0200

    admin: master: window: script: move vue component definitions into own file

--- admin/master/window/script.js
+++ admin/master/window/script.js
@@ -1,3 +1,15 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+let window_status = clone(shif_device);
+window_status.template = `
+    <shif-generic-l2 v-bind:icon="cond.icon.name"
+                     v-bind:title="dev.label"
+                     v-bind:active="{icon: cond.icon.color, text: cond.text.color}"
+                     v-bind:status="status"
+                     v-bind:place="place">
+    </shif-generic-l2>
+`;
+
+shif_comps_create('windowContact', window_status, window_status);
+shif_comps_create('windowHandle',  window_status, window_status);
