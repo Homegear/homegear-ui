d8656c257700cd75acb7a6f19345155c2aec2ad9
Marwin <git@marwinh.de>
2019-06-22 00:10:15 +0200

    admin: master: climate: script: implement openweathermap

--- admin/master/climate/script.js
+++ admin/master/climate/script.js
@@ -8,7 +8,7 @@ function status_format(status, precision) {
     }
 
     for (let cur of status)
-        cur.value = status_format.apply(this, [cur.value, precision])
+        cur.value = status_format.apply(this, [cur.value, precision]);
 
     return status;
 }
@@ -16,12 +16,17 @@ function status_format(status, precision) {
 
 let humidity = clone(shif_device);
 humidity.computed.status_formatted = function () {
-    return status_format.apply(this, [this.status, 1]);
+    return status_format.apply(this, [this.status_minimal(false), 1]);
+};
+humidity.computed.icon = function () {
+    return 'humidity' in this.icons
+        ? this.icons.humidity
+        : this.control.icons.humidity;
 };
 humidity.template = `
-    <shif-generic-l2 v-bind:icon="icons.humidity.name"
-                     v-bind:title="dev.label"
-                     v-bind:active="{icon: icons.humidity.color, text: texts.title.color}"
+    <shif-generic-l2 v-bind:icon="icon.name"
+                     v-bind:title="title"
+                     v-bind:active="{icon: icon.color, text: cond.text.color}"
                      v-bind:status="status_formatted"
                      v-bind:place="place">
     </shif-generic-l2>
@@ -29,16 +34,39 @@ humidity.template = `
 
 let temperature = clone(shif_device);
 temperature.computed.status_formatted = function () {
-    return status_format.apply(this, [this.status, 1]);
+    return status_format.apply(this, [this.status_minimal(false), 1]);
+};
+temperature.computed.icon = function () {
+    return 'temperature' in this.icons
+        ? this.icons.temperature
+        : this.control.icons.temperature;
 };
 temperature.template = `
-    <shif-generic-l2 v-bind:icon="icons.temperature.name"
-                     v-bind:title="dev.label"
-                     v-bind:active="{icon: icons.temperature.color, text: texts.title.color}"
+    <shif-generic-l2 v-bind:icon="icon.name"
+                     v-bind:title="title"
+                     v-bind:active="{icon: icon.color, text: texts.title.color}"
                      v-bind:status="status_formatted"
                      v-bind:place="place">
     </shif-generic-l2>
 `;
 
-shif_comps_create('humidity',    humidity,    humidity);
-shif_comps_create('temperature', temperature, temperature);
+
+let openweathermap_l2 = clone(shif_device);
+openweathermap_l2.computed.status_formatted = function () {
+    return status_format.apply(this, [this.status, 1]);
+};
+openweathermap_l2.template = `
+    <shif-generic-l2 v-bind:icon="icons.weather.name"
+                     v-bind:title="dev.label"
+                     v-bind:active="{icon: icons.weather.color, text: texts.title.color}"
+                     v-bind:status="status_formatted"
+                     v-bind:place="place"
+                     v-bind:actions="true"
+                     v-on:click="level3(device, breadcrumb)">
+    </shif-generic-l2>
+`;
+
+
+shif_comps_create('humidity',       openweathermap_l2,       humidity);
+shif_comps_create('temperature',    openweathermap_l2,    temperature);
+// shif_comps_create('OpenWeatherMap', openweathermap_l2, openweathermap_l2);
