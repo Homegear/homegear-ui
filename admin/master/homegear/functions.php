af5f12912e1f608d570aff2ccfaf6603a322ee90
Dr. Sathya Laufer <s.laufer@homegear.email>
2019-10-09 14:09:01 +0200

    Dynamically detect HTTPS

--- admin/master/homegear/functions.php
+++ admin/master/homegear/functions.php
@@ -4,6 +4,10 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 function homegear_init() {
     global $interfaceData;
+
+    if(!$_SERVER['WEBSOCKET_ENABLED']) die('WebSockets are not enabled on this server in "rpcservers.conf".');
+    if($_SERVER['WEBSOCKET_AUTH_TYPE'] != 'session') die('WebSocket authorization type is not set to "session" in "rpcservers.conf".');
+
     $hg = new \Homegear\Homegear();
 
     try {
