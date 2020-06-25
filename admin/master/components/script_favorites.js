1309b88fba04ab4b4264b1665b440acca7b4ea42
Marwin <git@marwinh.de>
2020-06-26 01:16:55 +0200

    admin: master: components: script_favorites: rework router params into component properties

--- admin/master/components/script_favorites.js
+++ admin/master/components/script_favorites.js
@@ -1,6 +1,20 @@
 let ShifFavoritesLvl3 = {
     mixins: [mixin_print_mounted('shif-favorites-level-3')],
 
+    props: {
+        room_id:   { required: true, },
+        device_id: { },
+        floor_id:  { required: true, },
+    },
+
+    provide: function () {
+        return {
+            floor_id:  this.floor_id,
+            device_id: this.device_id,
+            room_id:   this.room_id,
+        };
+    },
+
     template: `
         <shif-house-collected-entries v-bind:layer="3" />
     `
@@ -10,6 +24,20 @@ let ShifFavoritesLvl3 = {
 
 let ShifFavoritesLvl1 = {
     mixins: [mixin_print_mounted('shif-favorites-level-1')],
+
+    props: {
+        room_id:   { },
+        floor_id:  { },
+        device_id: { },
+    },
+
+    provide: function () {
+        return {
+            floor_id:  this.floor_id,
+            device_id: this.device_id,
+            room_id:   this.room_id,
+        };
+    },
 
     template: `
         <shif-house-collected-entries v-bind:layer="2"
