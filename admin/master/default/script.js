7ed1d432fb1a8a1d68daae63291bd2180e7f0bbb
Marwin <git@marwinh.de>
2020-08-04 11:09:55 +0200

    admin: master: default: script: clone: var -> let

--- admin/master/default/script.js
+++ admin/master/default/script.js
@@ -180,10 +180,11 @@ function set_or_extend(arr, idx, vals) {
 // Clone {{{
 // src: https://stackoverflow.com/a/728694
 function clone(obj) {
-    var copy;
+    let copy;
 
     // Handle the 3 simple types, and null or undefined
-    if (null == obj || 'object' != typeof obj) return obj;
+    if (null == obj || 'object' != typeof obj)
+        return obj;
 
     // Handle Date
     if (obj instanceof Date) {
@@ -195,16 +196,16 @@ function clone(obj) {
     // Handle Array
     if (obj instanceof Array) {
         copy = [];
-        for (var i = 0, len = obj.length; i < len; i++) {
+        for (let i = 0, len = obj.length; i < len; i++)
             copy[i] = clone(obj[i]);
-        }
+
         return copy;
     }
 
     // Handle Object
     if (obj instanceof Object) {
         copy = {};
-        for (var attr in obj) {
+        for (let attr in obj) {
             if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
         }
         return copy;
