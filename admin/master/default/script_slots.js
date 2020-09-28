ca0bbd55eadd61d96e087e44ddb19996c38364f5
Marwin <git@marwinh.de>
2020-09-28 09:54:53 +0200

    admin: master: default: script_slots: add file with default slot templates

---
+++ admin/master/default/script_slots.js
@@ -0,0 +1,45 @@
+/*
+    exported
+        shif_device_slot_automations
+        shif_device_slot_draggable
+        shif_device_slot_favorites
+        shif_device_slot_profiles
+*/
+
+
+
+const shif_device_slot_automations = `
+    <template v-slot:automations>
+        <router-link v-if="used_by_automations !== false"
+                     v-bind:to="automation_link">
+            <shif-icon src="calendar-time_1" />
+        </router-link>
+    </template>
+`;
+
+
+
+// TODO: Adapt icon class
+const shif_device_slot_draggable = `
+    <template v-slot:draggable>
+        <shif-icon src="move_1" classname="drag_drop_icon" />
+    </template>
+`;
+
+
+
+const shif_device_slot_favorites = `
+    <template v-slot:favorites>
+        <shif-checkbox-favorites v-bind:dev="dev" />
+    </template>
+`;
+
+
+
+const shif_device_slot_profiles = `
+    <template v-slot:profiles>
+        <shif-checkbox-profiles v-bind:dev="dev"
+                                v-bind:output="output"
+                                v-bind:props="props" />
+    </template>
+`;
