b5d5ebcd0bd0e0ee955ca63e7c241b2cc5d580f9
Marwin <git@marwinh.de>
2020-09-28 10:00:00 +0200

    admin: master: heating: script: replace slot snippets with variables

--- admin/master/heating/script.js
+++ admin/master/heating/script.js
@@ -2,8 +2,11 @@
     global
         clone
         homegear
-        shif_device
         shif_comps_create
+        shif_device
+        shif_device_slot_automations
+        shif_device_slot_favorites
+        shif_device_slot_profiles
         shif_register_disable_hooks
 */
 
@@ -25,12 +28,9 @@ heating_slider_l2.template = `
                      v-bind:actions="true"
                      v-on:click_icon="change"
                      v-on:click="level3(device)">
-        <template v-slot:automations>
-            <router-link v-if="used_by_automations !== false"
-                         v-bind:to="automation_link">
-                <shif-icon src="calendar-time_1" />
-            </router-link>
-        </template>
+
+        ${shif_device_slot_automations}
+
     </shif-generic-l2>
 `;
 let heating_slider_l3 = clone(shif_device);
@@ -48,18 +48,10 @@ heating_slider_l3.template = `
                  v-on:change="change"
                  v-bind:disabled="disabled"
                  v-model:value="props.value">
-        <template v-slot:profiles>
-            <shif-checkbox-profiles v-bind:dev="dev"
-                                    v-bind:output="output"
-                                    v-bind:props="props" />
-        </template>
 
-        <template v-slot:automations>
-            <router-link v-if="used_by_automations !== false"
-                         v-bind:to="automation_link">
-                <shif-icon src="calendar-time_1" />
-            </router-link>
-        </template>
+        ${shif_device_slot_profiles}
+        ${shif_device_slot_automations}
+
     </shif-slider>
 `;
 
@@ -77,16 +69,9 @@ heating_is_state_l2.template = `
                      v-bind:place="place"
                      v-on:click="level3(device)">
 
-        <template v-slot:favorites>
-            <shif-checkbox-favorites v-bind:dev="dev" />
-        </template>
+        ${shif_device_slot_favorites}
+        ${shif_device_slot_automations}
 
-        <template v-slot:automations>
-            <router-link v-if="used_by_automations !== false"
-                         v-bind:to="automation_link">
-                <shif-icon src="calendar-time_1" />
-            </router-link>
-        </template>
     </shif-generic-l2>
 `;
 let heating_is_state_l3 = clone(shif_device);
@@ -96,12 +81,9 @@ heating_is_state_l3.template = `
                      v-bind:active="{icon: control.icons.temperature.color, text: texts.title.color}"
                      v-bind:status="status"
                      v-bind:place="place">
-        <template v-slot:automations>
-            <router-link v-if="used_by_automations !== false"
-                         v-bind:to="automation_link">
-                <shif-icon src="calendar-time_1" />
-            </router-link>
-        </template>
+
+        ${shif_device_slot_automations}
+
     </shif-generic-l2>
 `;
 
@@ -126,18 +108,10 @@ heating_mode_l3.template = `
                 v-bind:values="values"
                 v-bind:disabled="disabled"
                 v-on:input="x => change(parseInt(x))">
-        <template v-slot:profiles>
-            <shif-checkbox-profiles v-bind:dev="dev"
-                                    v-bind:output="output"
-                                    v-bind:props="props" />
-        </template>
 
-        <template v-slot:automations>
-            <router-link v-if="used_by_automations !== false"
-                         v-bind:to="automation_link">
-                <shif-icon src="calendar-time_1" />
-            </router-link>
-        </template>
+        ${shif_device_slot_profiles}
+        ${shif_device_slot_automations}
+
     </shif-radio>
 `;
 
@@ -152,12 +126,9 @@ heating_window.template = `
                      v-bind:active="{icon: cond.icon.color, text: cond.text.color}"
                      v-bind:status="status_minimal()"
                      v-bind:place="place">
-        <template v-slot:automations>
-            <router-link v-if="used_by_automations !== false"
-                         v-bind:to="automation_link">
-                <shif-icon src="calendar-time_1" />
-            </router-link>
-        </template>
+
+        ${shif_device_slot_automations}
+
     </shif-generic-l2>
 `;
 
