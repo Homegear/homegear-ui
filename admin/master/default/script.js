64915a93eec56aa37b9120caae0080c59811efc1
Marwin <git@marwinh.de>
2020-09-30 03:55:06 +0200

    admin: master: default: script: add condition to draggable icon

--- admin/master/default/script.js
+++ admin/master/default/script.js
@@ -1111,7 +1111,8 @@ Vue.component('shif-room', {
             <router-link v-bind:to="link(floor.key, room)">
                 <shif-icon v-bind:src="interfaceData.rooms[room].icon"
                            class="roomSelect" />
-                <shif-icon src="move_2" classname="drag_drop_icon_right" />
+                <shif-icon v-if="$root.draggable.enabled"
+                           src="move_2" classname="drag_drop_icon_right" />
                 <div class="description">
                     {{ interfaceData.rooms[room].name }}
                 </div>
