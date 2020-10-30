0dedf438e7e60776c610da29d6a2322624c7adef
Marwin <git@marwinh.de>
2020-10-30 10:35:53 +0100

    admin: master: climate: script: round floating point numbers for HeatingIsState*

--- admin/master/climate/script.js
+++ admin/master/climate/script.js
@@ -6,6 +6,7 @@
         shif_device_slot_automations
         shif_device_slot_draggable
         shif_device_slot_favorites
+        status_format
 */
 
 
@@ -13,20 +14,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-function status_format(status, precision) {
-    if (typeof(status) === 'string') {
-        const regex = /^(\d+(?:[.,]\d+)?)/;
-        return status.replace(regex, (match) => this.float_formatted(match, precision));
-    }
-
-    for (let cur of status)
-        cur.value = status_format.apply(this, [cur.value, precision]);
-
-    return status;
-}
-
-
-
 let climate_main = clone(shif_device);
 climate_main.computed.status_formatted = function () {
     return status_format.apply(this, [this.status_minimal(false), 1]);
