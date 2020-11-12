345ab9f015c9c1ff30badf5d93f233ba5b58c99f
Marwin <git@marwinh.de>
2020-11-12 13:07:54 +0100

    admin: master: default: script_states: refactor of devices and device aggregations from roles into categories

--- admin/master/default/script_states.js
+++ admin/master/default/script_states.js
@@ -1,8 +1,8 @@
 /*
     global
+        app
         get_or_default
         homegear
-        set_or_extend
 */
 /*
     exported
@@ -20,52 +20,42 @@ let _states = Vue.observable({
 
 
 
-function _status_gather_invokes(role, role_id) {
-    let ret = [{
-        type: get_or_default(role, 'aggregationType', 2),
-        ids:  [{'id':role_id,'direction':0}],
-    }];
-
-    if (!('rolesInclude' in role))
-        return ret;
-
-    for (const role_inc of role.rolesInclude) {
-        // TODO: test this refactoring
-        // const ids = role_inc.roles.map(x => ({
-            // id: x,
-            // direction: 0
-        // }));
-        let ids = [];
-        for(const index in role_inc.roles) {
-            ids.push({
-                id: role_inc.roles[index],
-                direction: 0,
-            });
-        }
-
-        ret.push({
-            type: get_or_default(role_inc, 'aggregationType', 2),
-            ids:  ids,
-        });
-    }
-
-    return ret;
+function _status_gather_invokes(category) {
+    return get_or_default(category, 'aggregation', [])
+                .map(cat_inc => ({
+                    type: get_or_default(cat_inc, 'type', 2),
+                    ids:  cat_inc.roles.map(x => ({id: x})),
+                 }));
 }
 
 
 
-function _status_text(role, texts_idx) {
-    if (role.texts === undefined)
+function _status_text(category, idx) {
+    if (category.statusMap === undefined ||
+        category.statusMap[idx] === undefined)
         return '?';
 
-    if ((typeof(texts_idx) === 'number' && role.texts.length > texts_idx) ||
-        (typeof(texts_idx) === 'string' && texts_idx in role.texts))
-        return role.texts[texts_idx];
+    return category.statusMap[idx];
+}
 
-    if (role.texts.length == 1)
-        return role.texts[0];
 
-    return '?';
+
+function _status_from_map(category) {
+    let out = {};
+
+    for (const key in category.statusMap)
+        out[_status_text(category, key)] = 0;
+
+    return out;
+}
+
+
+
+function _status_array_form_map(map) {
+    return Object.keys(map)
+                 .map(x => ({
+                     key: x, value: map[x]
+                  }));
 }
 
 
@@ -79,97 +69,64 @@ function _invoke(op) {
 }
 
 
-
-function status_role(role_id) {
+function status_category(cat_id) {
     return new Promise(function (resolve) {
-        const role = interfaceData.roles[role_id];
-        const invokes_descs = _status_gather_invokes(role, role_id);
+        const category = interfaceData.deviceCategories[cat_id];
+        const invokes_descs = _status_gather_invokes(category);
 
-        if ('l2_status' in role) {
-            _states.content[role_id] = [];
+        if (category.l2_status !== true) {
+            Vue.set(_states.content, cat_id, []);
             return resolve();
         }
 
-        for (const invoke_desc of invokes_descs) {
-            const invoke_op = {
+        let new_ = _status_from_map(category);
+        Promise.allSettled(
+            invokes_descs.map(x => ({
                 jsonrpc: '2.0',
                 method: 'aggregateRoles',
-                params: [invoke_desc.type, invoke_desc.ids, [], 0, true],
-            };
-            homegear.invoke(invoke_op, function (res) {
-                /**
-                * Updating this is a two step process:
-                * 1) We need to fetch the new values from upstream.
-                * 2) We need to zero out the outdated old values.
-                **/
-                const keys = Object.keys(res.result)
-                                    .filter(x => x !== 'variableCount');
-                for (const key of keys) {
-                    /**
-                    * We have to be very careful here, to not break Vue's
-                    * reactiveness.
-                    * 1) In case we need to insert a value, push is needed.
-                    * 2) In case we need to change a value, direct
-                    *    assignment of the single object members at
-                    *    respective array index is needed.
-                    **/
-                    for (let i = 0; i < _states.content[role_id].length; ++i) {
-                        if (_states.content[role_id][i].raw == key) {
-                            set_or_extend(_states.content[role_id], i, {
-                                key:   _status_text(role, key),
-                                value: res.result[key],
-                                raw:   key,
-                            });
-                            break;
-                        }
-                    }
-                }
+                params: [x.type, x.ids, [], 0, true],
+            })).map(_invoke)
+        ).then(function (results) {
+            if (app.debug)
+                console.log(results);
 
-                for (let j = _states.content[role_id].length - 1; j >= 0; --j) {
-                    if (keys.indexOf(_states.content[role_id][j].raw) === -1)
-                        _states.content[role_id][j].value = 0;
-                }
-            });
-        }
+            for (const result of results) {
+                if (result.status === 'rejected')
+                    continue;
 
-        return resolve();
+                for (const field of Object.keys(result.value.result)
+                                          .filter(x => x !== 'variableCount')) {
+                    const key = _status_text(category, field);
+                    if (key === '?')
+                        continue;
+
+                    if (new_[key] === undefined)
+                        new_[key] = 0;
+
+                    new_[key] += result.value.result[field];
+                }
+            }
+        }).then(function () {
+            Vue.set(_states.content, cat_id, _status_array_form_map(new_));
+        }).then(resolve());
     });
 }
 
 
 
 function states_init() {
-    const keys = Object.keys(interfaceData.roles).map(Number);
+    const cat_keys = Object.keys(interfaceData.deviceCategories);
 
-    for (const key of keys) {
-        /**
-         * Always reinitialize to an empty array here.
-         * In case of a homegear reconnect, we would duplicate the
-         * status text otherwise.
-         **/
-        Vue.set(_states.content, key, []);
-
-        const role = interfaceData.roles[key];
-        if (!('texts' in role))
+    for (const key of cat_keys) {
+        const category = interfaceData.deviceCategories[key];
+        if (category.statusMap === undefined)
             continue;
 
-        /**
-         * This must be a for-in loop! Do not change it into a
-         * traditional for loop or a map.
-         * We need iterate over both, integer keys, as well as object
-         * keys (iteration order does not matter for the integer keys).
-         **/
-        for (const text_idx in role.texts)
-            _states.content[key].push(
-                {
-                    key:   _status_text(role, text_idx),
-                    raw:   text_idx + '',
-                    value: 0,
-                }
-            );
+        const map = _status_from_map(category);
+        Vue.set(_states.content, key, _status_array_form_map(map));
     }
 
-    Promise.allSettled(keys.map(key => status_role(key)))
+    Promise.allSettled(cat_keys.map(status_category))
            .then(() => _states.initialized = true);
 }
 homegear.ready(states_init);
@@ -206,7 +163,7 @@ const mixin_states = {
             const dirty = _states.dirty.slice();
             _states.dirty = [];
 
-            Promise.allSettled(dirty.map(x => status_role(x)));
+            Promise.allSettled(dirty.map(x => status_category(x)));
         },
     },
 };
