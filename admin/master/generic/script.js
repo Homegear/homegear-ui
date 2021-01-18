c25f1eb57cebcdefbcdfaa391ee139f64a0f3c8e
Dr. Sathya Laufer <s.laufer@homegear.email>
2021-01-18 21:18:41 +0100

    Added generic slider

--- admin/master/generic/script.js
+++ admin/master/generic/script.js
@@ -39,16 +39,42 @@ generic_status.template = `
 
 shif_comps_create('genericStatus', generic_status, generic_status);
 
-let generic_switch_l2 = clone(shif_device);
-generic_switch_l2.template = `
+let generic_status_link = clone(shif_device);
+generic_status_link.computed.status_formatted = function () {
+    var precision = 0;
+    if (this.dynamicMetadata.hasOwnProperty("precision")) {
+        precision = this.dynamicMetadata.precision;
+    }
+    return status_format.apply(this, [this.status_minimal(false), precision]);
+};
+generic_status_link.computed.icon = function () {
+    return 'main' in this.icons
+        ? this.icons.main
+        : this.control.icons.main;
+};
+generic_status_link.template = `
+    <shif-generic-l2 v-bind:icon="icon.name"
+                     v-bind:title="dev.label"
+                     v-bind:active="{icon: icon.color, text: texts.title.color}"
+                     v-bind:status="status_formatted"
+                     v-bind:place="place"
+                     v-bind:actions="true"
+                     v-on:click="level3(device)">
+
+        ${shif_device_slot_automations}
+        ${shif_device_slot_draggable}
+
+    </shif-generic-l2>
+`;
+
+let generic_switch = clone(shif_device);
+generic_switch.template = `
     <shif-generic-l2 v-bind:icon="cond.icon.name"
                      v-bind:title="dev.label"
                      v-bind:active="{icon: cond.icon.color, text: cond.text.color}"
                      v-bind:status="status"
                      v-bind:place="place"
-                     v-bind:actions="true"
-                     v-on:click_icon="$homegear.value_set(output, ! props.value)"
-                     v-on:click="level3(device)">
+                     v-on:click_icon="$homegear.value_set(output, ! props.value)">
 
         ${shif_device_slot_profiles}
         ${shif_device_slot_automations}
@@ -57,21 +83,38 @@ generic_switch_l2.template = `
     </shif-generic-l2>
 `;
 
-let generic_switch_l3 = clone(shif_device);
-generic_switch_l3.template = `
-    <shif-generic-l2 v-bind:icon="cond.icon.name"
-                     v-bind:title="title"
-                     v-bind:active="{icon: cond.icon.color, text: cond.text.color}"
-                     v-bind:place="place"
-                     v-bind:status="status_minimal()"
-                     v-on:click="$homegear.value_set(output, ! props.value)">
+shif_comps_create('genericSwitch', generic_switch, generic_switch);
 
-        ${shif_device_slot_favorites}
+let generic_slider = clone(shif_device);
+generic_slider.computed.status_formatted = function () {
+    var decimalPlaces = 0;
+    if (this.dynamicMetadata.hasOwnProperty("stepSize")) {
+        var stepSize = this.dynamicMetadata.stepSize;
+        if (stepSize !== Math.floor(stepSize)) {
+            decimalPlaces = stepSize.toString().split(".")[1].length || 0;
+            console.log(decimalPlaces);
+        }
+    }
+    return status_format.apply(this, [this.status_minimal(false), decimalPlaces]);
+};
+generic_slider.template = `
+    <shif-slider v-bind:min="props.minimumScaled"
+                 v-bind:max="props.maximumScaled"
+                 v-bind:unit="props.unit"
+                 v-bind:value="props.value"
+                 v-bind:title="title"
+                 v-bind:step="dynamicMetadata.stepSize ?? 1.0"
+                 v-bind:precision="dynamicMetadata.precision ?? 0"
+                 v-bind:disabled="disabled"
+                 v-on:change="$homegear.value_set(output, props.value)"
+                 v-model:value="props.value">
+
         ${shif_device_slot_profiles}
         ${shif_device_slot_automations}
         ${shif_device_slot_draggable}
 
-    </shif-generic-l2>
+
+    </shif-slider>
 `;
 
-shif_comps_create('genericSwitch', generic_switch_l2, generic_switch_l3);
+shif_comps_create('genericSlider', generic_status_link, generic_slider);
